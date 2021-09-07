c4cfc7fe63d4cacb243b8cb96b67716570be5e65
Riccardo Francesco Croce <riccardocroce129@outlook.com>
2021-09-07 12:54:53 +0200

    Adding i18n implementation, + 3 language support (EN, IT, JP)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,11 @@
 import { Component, HostBinding, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { TranslateService } from '@ngx-translate/core';
 
+interface Language {
+  id: string,
+  desc: string
+}
 @Component({
   // Change with app-root to access body of page
   selector: 'body',
@@ -15,11 +20,36 @@ export class AppComponent implements OnInit {
   // Example of State
   count = 0;
 
+  language :string = 'EN';
+  // Language Array
+  languages: Language[] = [
+    {
+      id: "JP", 
+      desc: "Japan 🇯🇵" 
+    },
+    {
+      id: "EN", 
+      desc: "United Kingdom 🇬🇧" 
+    },
+    {
+      id: "IT", 
+      desc: "Italy 🇮🇹" 
+    },
+  ]
+
+  constructor(private translate: TranslateService){
+    this.translate.addLangs(['JP', 'EN', 'IT']);
+    this.translate.use(this.language);
+  }
+
   ngOnInit(): void {
     this.toggleTheme.valueChanges.subscribe((theme)=>{
       const darkTheme = 'dark';
       this.class = theme ? darkTheme:'';
     })
+  }
+  changeLanguage(language: any){
+    this.translate.use(language);
   }
   // Example counter
   counter(){
